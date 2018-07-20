b7f200748c71aa5c2a39312b5b8ea6ef99c4d993
souppower <r.kubota92@gmail.com>
2018-07-20 13:56:19 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# graphql-rest-api-wrapper
+
+You can find more info about this repository [here](https://medium.com/@graphcool/how-to-wrap-a-rest-api-with-graphql-8bf3fb17547d).
+
+## Usage
+
+```sh
+$ git clone git@github.com:souppower/graphql-rest-wrapper.git
+$ cd graphql-rest-wrapper
+$ yarn
+$ yarn start
+```
---
+++ src/index.js
@@ -0,0 +1,30 @@
+const { GraphQLServer } = require('graphql-yoga')
+const fetch = require('node-fetch')
+
+const baseURL = `https://jsonplaceholder.typicode.com`
+
+const resolvers = {
+  Query: {
+    users: () => {
+      return fetch(`${baseURL}/users`).then(res => res.json())
+    },
+    user: (parent, args) => {
+      const { id } = args
+      return fetch(`${baseURL}/users/${id}`).then(res => res.json())
+    },
+    posts: () => {
+      return fetch(`${baseURL}/posts`).then(res => res.json())
+    },
+    post: (parent, args) => {
+      const { id } = args
+      return fetch(`${baseURL}/posts/${id}`).then(res => res.json())
+    },
+  }
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+})
+
+server.start(() => console.log(`Server is running on http://localhost:4000`))
